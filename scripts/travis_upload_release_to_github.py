676d4fc17fe8d270783ff1811abd2052ef37ca1a
unknown <tilikin@acdlab26.ACD_NT>
2015-08-04 12:56:01 +0300

    fix

--- scripts/travis_upload_release_to_github.py
+++ scripts/travis_upload_release_to_github.py
@@ -97,11 +97,11 @@ import sh
 git = sh.git.bake(_cwd='./')
 print git.status()
 # checkout and track a remote branch
-print git.checkout('-b', 'somebranch')
+#print git.checkout('-b', 'somebranch')
 # add a file
-print git.add('somefile')
+#print git.add('somefile')
 # commit
-print git.commit(m='my commit message')
+#print git.commit(m='my commit message')
 # now we are one commit ahead
 print git.status()
 
