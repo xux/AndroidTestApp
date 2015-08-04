7d35a00580bca9bf06fb8bf68b949573d1713a48
unknown <tilikin@acdlab26.ACD_NT>
2015-08-04 11:14:10 +0300

    fix

--- create_keystore.bat
+++ create_keystore.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jre7\bin\keytool.exe" -genkey -validity 10000 -dname "CN=AndroidDebug, O=Android, C=US" -keystore AndroidTest.keystore -storepass android -keypass android -alias androiddebugkey -keyalg RSA -v -keysize 2048
+"C:\Program Files\Java\jre7\bin\keytool.exe" -genkey -validity 10000 -dname "CN=AndroidDebug, O=Android, C=US" -keystore app\AndroidTest.keystore -storepass android -keypass android -alias androiddebugkey -keyalg RSA -v -keysize 2048
